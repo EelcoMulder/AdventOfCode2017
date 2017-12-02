4316e2a9bf388d854e9ff9923e9ba74c8a8bde09
Eelco Mulder <eelcomulder@hotmail.com>
2017-12-02 17:12:40 +0100

    Day 1 Part 2... something is wrong

--- Day1/Day1.Test/Tests.fs
+++ Day1/Day1.Test/Tests.fs
@@ -1,7 +1,6 @@
 module Tests
 
 open Day1
-open System
 open Xunit
 
 [<Theory>]
@@ -9,6 +8,16 @@ open Xunit
 [<InlineData("1111", 4)>]
 [<InlineData("1234", 0)>]
 [<InlineData("91212129", 9)>]
-let ``Day1_WhenParsingInput_ItShouldReturnCorrectResult`` (input, expected) =
-    let result = calculateCaptcha input
+let ``Day1_Part1_WhenParsingInput_ItShouldReturnCorrectResult`` (input, expected) =
+    let result = calculateCaptchaPart1 input
+    Assert.Equal(expected, result)
+
+[<Theory>]
+[<InlineData("1212", 6)>]
+[<InlineData("1221", 0)>]
+[<InlineData("123425", 4)>]
+[<InlineData("123123", 12)>]
+[<InlineData("12131415", 4)>]
+let ``Day1_Part2_WhenParsingInput_ItShouldReturnCorrectResult`` (input, expected) =
+    let result = calculateCaptchaPart2 input
     Assert.Equal(expected, result)
--- Day1/Day1/Day1.fs
+++ Day1/Day1/Day1.fs
@@ -4,22 +4,29 @@ open System.Text
 let getIndexValue index step digits =
     match (index < Array.length digits - 1) && digits.[index] = digits.[index + step] with
     | true -> digits.[index]
-    | false -> 0uy
+    | false -> 0
 
-let calculateCaptcha (digitsString) =
-    let bytes = digitsString 
-                |> Seq.map (fun d -> (System.Char.GetNumericValue d) |> byte)
-                |> Seq.toArray
+let convertToIntArray d = 
+    d |> Seq.map (fun d -> (System.Char.GetNumericValue d) |> int)
+    |> Seq.toArray    
 
-    let cv = getIndexValue 0 ((Array.length bytes) - 1) bytes
+let calculateCaptchaPart1 (digitsString : string) =
+    let digits = convertToIntArray digitsString 
+
+    let cv = getIndexValue 0 ((Array.length digits) - 1) digits |> int
     
     let total =
-        bytes |> Seq.mapi (fun idx _ -> getIndexValue idx 1 bytes)
-        |> Seq.sum
-    (total + cv)
+        digits |> Seq.mapi (fun idx _ -> getIndexValue idx 1 digits)
+        |> Seq.sum |> int
+    total + cv
 
 // use local functions
-// Part 2!!
-
-
+let calculateCaptchaPart2 (digitsString : string) = 
+    let digits = convertToIntArray digitsString 
+    let step = (Array.length digits) / 2
+    let doubleByteArray = Array.append digits digits
+    let total =
+        digits |> Seq.mapi (fun idx _ -> getIndexValue idx step doubleByteArray)
+        |> Seq.sum
+    total
 
