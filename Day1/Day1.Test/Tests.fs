8c02dce6170c31bf2fe62cd1b6a24b054b1ef303
Eelco Mulder <eelcomulder@hotmail.com>
2017-12-02 16:52:24 +0100

    Refactor...

--- Day1/Day1.Test/Tests.fs
+++ Day1/Day1.Test/Tests.fs
@@ -5,10 +5,10 @@ open System
 open Xunit
 
 [<Theory>]
-[<InlineData(1122, 3)>]
-[<InlineData(1111, 4)>]
-[<InlineData(1234, 0)>]
-[<InlineData(91212129, 9)>]
+[<InlineData("1122", 3)>]
+[<InlineData("1111", 4)>]
+[<InlineData("1234", 0)>]
+[<InlineData("91212129", 9)>]
 let ``Day1_WhenParsingInput_ItShouldReturnCorrectResult`` (input, expected) =
     let result = calculateCaptcha input
     Assert.Equal(expected, result)
--- Day1/Day1/Day1.fs
+++ Day1/Day1/Day1.fs
@@ -1,17 +1,25 @@
 module Day1
+open System.Text
 
-let determineValue (index : int, numberArray : string) =
-    match (index < String.length numberArray - 1) && numberArray.[index] = numberArray.[index + 1] with
-    | true -> System.Char.GetNumericValue numberArray.[index]
-    | false -> 0.0
+let getIndexValue index step digits =
+    match (index < Array.length digits - 1) && digits.[index] = digits.[index + step] with
+    | true -> digits.[index]
+    | false -> 0uy
 
-let calculateCaptcha (i : string) =
-    let cv = 
-        match i.[0] = i.[String.length i - 1] with
-        | true ->  (System.Char.GetNumericValue i.[0])
-        | false -> 0.0
+let calculateCaptcha (digitsString) =
+    let bytes = digitsString 
+                |> Seq.map (fun d -> (System.Char.GetNumericValue d) |> byte)
+                |> Seq.toArray
 
+    let cv = getIndexValue 0 ((Array.length bytes) - 1) bytes
+    
     let total =
-        i |> Seq.mapi (fun idx _ -> determineValue(idx, i)) 
+        bytes |> Seq.mapi (fun idx _ -> getIndexValue idx 1 bytes)
         |> Seq.sum
-    (total + cv) |> string
+    (total + cv)
+
+// use local functions
+// Part 2!!
+
+
+
